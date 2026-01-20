b6ff2996ded39ad6130a1e835926c83a21a3defb
MD Saad <mdsaad8081@gmail.com>
2026-01-20 12:13:12 +0530

    Cv name Updated

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -30,7 +30,7 @@ export const AboutSection = () => {
               </a>
 
               <a
-                href="\CV.pdf" download
+                href="\Saad-Software-Engineer-CV.pdf" download
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
               >
                 Download CV
